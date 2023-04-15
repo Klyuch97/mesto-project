df6a17370425f873ebb2ef2f2c765770967ce22e
Nikita Kliuchnikov <klyuch1994@mail.ru>
2023-04-15 21:39:38 +0500

    editAvatar

--- src/index.js
+++ src/index.js
@@ -19,20 +19,25 @@ const buttonCloseAddCardPopup = document.querySelector('.popup__close_add_image'
 const elementList = document.querySelector('.elements');
 const templateElements = document.querySelector('#templateElements').content;
 const formAddImage = document.querySelector('.form_add-image');
-const nameInputImage = formAddImage.querySelector('.form_name-image');
-const linkInputImage = formAddImage.querySelector('input:nth-of-type(2)');
 const nameProfile = document.querySelector('.profile__info-cell-text');
 const job = document.querySelector('.profile__info-text');
 const buttonCloseImagePopup = document.querySelector('.popup__close_image');
 const formElement = document.querySelector('.form');
+const buttonEditAvatar = document.querySelector('.profile__edit-avatar-button');
+const popupEditAvatar = document.querySelector('.popup_edit-avatar');
+const formEditAvatar = document.querySelector('.form_edit-avatar')
+const linkAvatar = document.querySelector('.form_avatar');
 
 
 enableValidation(settings);
 
 function toggleButton(settings) {
   const buttonElementCreate = document.querySelector('.form__button__create');
+  const buttonEditAvatar = document.querySelector('.form__button-edit-avatar');
   buttonElementCreate.disabled = true;
   buttonElementCreate.classList.add(settings.formButtonDisabled);
+  buttonEditAvatar.disabled = true;
+  buttonEditAvatar.classList.add(settings.formButtonDisabled);
 }
 
 buttonOpenEditProfilePopup.addEventListener('click', function () {
@@ -45,6 +50,12 @@ buttonOpenAddCardPopup.addEventListener('click', function () {
   formAddImage.reset();
   openPopup(popupAddImage);
   toggleButton(settings);
+});
+
+buttonEditAvatar.addEventListener('click', function () {
+  formEditAvatar.reset();
+  toggleButton(settings);
+  openPopup(popupEditAvatar);
 });
 
 // находим все крестики проекта по универсальному селектору
@@ -66,5 +77,13 @@ initialCards.forEach(function (element) {
 
 formAddImage.addEventListener('submit', addCard);
 
+export function editAvatar(evt) {
+  evt.preventDefault();
+  const avatar = document.querySelector('.profile__image');
+  //const linkAvatar= document.querySelector('.form_avatar');
+  const linkAvatarValue = linkAvatar.value
+  avatar.src = linkAvatarValue;
+  closePopup(popupEditAvatar);
+}
 
-
+formEditAvatar.addEventListener('submit', editAvatar);
