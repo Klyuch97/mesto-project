874c82026c51a92f2785d01a2e0186e4f955f163
AKuznetsov <artemkuznetsov07@gmail.com>
2023-05-27 10:51:52 +0300

    константы, юзер инфо, индекс ксс

--- src/components/Userinfo.js
+++ src/components/Userinfo.js
@@ -1,24 +1,23 @@
-export default class UserInfo {
+export class UserInfo {
   //Получил селекторы с имeнем,информацией и аватаром
-  constructor({ userNameSelector, userAboutSelector, avatarSelector }) {
-    this.userName = document.querySelector(userNameSelector);
-    this.userAbout = document.querySelector(userAboutSelector);
-    this.avatar = document.querySelector(avatarSelector);
+  constructor(userInfoSettings) {
+    this._profileName = document.querySelector(userInfoSettings.profileNameSelector);
+    this._profileAbout = document.querySelector(userInfoSettings.profileAboutSelector);
+    this._profileAvatar = document.querySelector(userInfoSettings.profileAvatarSelector);
   }
-
+  // получить данные из профиля
   getUserInfo() {
-    return {
-      name: this.userName.textContent,
-      about: this.userAbout.textContent,
-      avatar: this.avatar.src,
-    };
+    return this._data;
   }
-
+  //добавить информацию в профиль из формы вместе с моим id
   setUserInfo(data) {
-    this._myId = data._id;
-    this.userName.textContent = data.name;
-    this.userAbout.textContent = data.about;
-    this.avatar.src = data.avatar;
+    this._data = data;
+    this._profileName.textContent = data.name;
+    this._profileAbout.textContent = data.about;
+    this._profileAvatar.src = data.avatar;
   };
+  //вернуть мой id
+  getId() {
+    return this._data._id;
+  }
 }
-
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,3 +1,35 @@
-
+'use strict';
+export const userInfoSettings = {
+  profileNameSelector: ".profile__info-cell-text",
+  profileAboutSelector: ".profile__info-text",
+  profileAvatarSelector: ".profile__image"
+}
+export const validatorSettings = {
+  form: '.form',
+  formNameText: '.form__name-text',
+  formButton: '.form__button',
+  formNameTextTypeError: 'form__name-text_type_error',
+  formInputErrorActive: 'form__input-error_active',
+  formButtonDisabled: 'form__button_disabled',
+};
+export const cardSectionConfig = {
+  deleteConfirmationSelector: '.popup_confirm-delete',
+  cardViewSelector: '.popup_open-card',
+  cardTemplate: '#templateElements'
+}
+export const apiConfig = {
+  baseUrl: 'https://mesto.nomoreparties.co/v1/plus-cohort-23',
+  headers: {
+    authorization: '70b0f800-c3d5-43c3-9a38-db0198e51959',
+    'Content-Type': 'application/json',
+  }
+};
+export const cardConfig = {
+  likeButtonSelector: '.element__button',
+  likeCounterSelector: '.element__likes-number',
+  imageSelector: '.element__image',
+  cardNameTextSelector: '.element__text',
+  removeButtonSelector: '.element__button-trash'
+}
 
 
