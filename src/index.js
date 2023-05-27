b9b0ece39de07b7e04df9b898a1738e16466b3dd
AKuznetsov <artemkuznetsov07@gmail.com>
2023-05-27 10:31:01 +0300

    слил индекс нтмл\джс

--- src/index.js
+++ src/index.js
@@ -1,240 +1,105 @@
 import '../src/index.css';
-import { openPopup, closePopup } from "./components/modal.js";
-import { enableValidation, settings } from "./components/validate.js";
-import Api from "./components/api.js";
-import Card from "./components/card.js";
-import Section from "./components/section.js";
-import UserInfo from './components/userinfo.js';
+import { webApi, profileInfo, cardSectionSingleton, formsValidationService, popupManagerSingleton as popupManager } from './components/utils.js';
 
+class IndexPagePresenter {
+  constructor() {
 
-const buttonOpenEditProfilePopup = document.querySelector('.profile__info-cell-button');
-const popupEdifProfile = document.querySelector('.popup_edit-profile');
-const nameInput = document.querySelector('.form__name-text');
-const jobInput = document.querySelector('input:nth-of-type(2)');
-const buttonOpenAddCardPopup = document.querySelector('.profile__button');
-const formAddImage = document.querySelector('.form_add-image');
-const nameProfile = document.querySelector('.profile__info-cell-text');
-const job = document.querySelector('.profile__info-text');
-const formElement = document.querySelector('.form');
-const buttonEditAvatar = document.querySelector('.profile__edit-avatar-button');
-const popupEditAvatar = document.querySelector('.popup_edit-avatar');
-const formEditAvatar = document.querySelector('.form_edit-avatar')
-const linkAvatar = document.querySelector('.form_avatar');
-const closeButtons = document.querySelectorAll('.popup__close');
-export const buttonElementCreate = document.querySelector('.form__button__create');
-const buttonEditAvatarSave = document.querySelector('.form__button-edit-avatar');
-export const buttonSaveProfile = document.querySelector('.form__button');
-export const buttonSaveAvatar = document.querySelector('.form__button-edit-avatar');
-const nameInputImage = formAddImage.querySelector('.form_name-image');
-const linkInputImage = formAddImage.querySelector('input:nth-of-type(2)');
-const elementList = document.querySelector('.elements');
-const popupAddImage = document.querySelector('.popup_add_image');
-const popupOpenCard = document.querySelector('.popup_open-card');
-const popupTextImage = popupOpenCard.querySelector('.popup__text-image');
-const popupImage = popupOpenCard.querySelector('.popup__image');
+    //profile
+    this._buttonOpenEditProfilePopup = document.querySelector('.profile__info-cell-button');
 
-/*function editAvatarInfo(avatar, name, about) {
-  document.querySelector('.profile__info-cell-text').textContent = name
-  document.querySelector('.profile__info-text').textContent = about
-  document.querySelector('.profile__image').src = avatar
-}
+    //popups
+    this._editProfileSelector = '.popup_edit-profile';
+    this._addImageSelector = '.popup_add_image';
+    this._editAvatarSelector = '.popup_edit-avatar';
 
-function toggleButton(settings, button) {
-  button.disabled = true;
-  button.classList.add(settings.formButtonDisabled);
-}
+    ///
+    this._buttonAddCard = document.querySelector('.profile__button');
+    this._buttonEditAvatar = document.querySelector('.profile__edit-avatar-button');
+    this._buttonEditAvatarSave = document.querySelector('.form__button-edit-avatar');
+    this._elementList = document.querySelector('.elements');
 
-buttonOpenEditProfilePopup.addEventListener('click', function () {
-  openPopup(popupEdifProfile);
-  nameInput.value = nameProfile.textContent;
-  jobInput.value = job.textContent;
-});
-
-buttonOpenAddCardPopup.addEventListener('click', function () {
-  formAddImage.reset();
-  openPopup(popupAddImage);
-  toggleButton(settings, buttonElementCreate);
-});
-buttonEditAvatar.addEventListener('click', function () {
-  formEditAvatar.reset();
-  toggleButton(settings, buttonEditAvatarSave);
-  openPopup(popupEditAvatar);
-});*/
-
-closeButtons.forEach((button) => {
-  // находим 1 раз ближайший к крестику попап
-  const popup = button.closest('.popup');
-  // устанавливаем обработчик закрытия на крестик
-  button.addEventListener('click', () => closePopup(popup));
-});
-
-
-//formAddImage.addEventListener('submit', addCard);
-
-/*function editAvatar(evt) {
-  evt.preventDefault();
-  renderLoading(true, buttonSaveAvatar);
-  avatarInfoPatch({ avatar: linkAvatar.value })
-    .then(data => {
-      const avatar = document.querySelector('.profile__image');
-      const linkAvatarValue = linkAvatar.value
-      avatar.src = linkAvatarValue;
-      closePopup(popupEditAvatar);
-    })
-    //Здесь вносим изменения в DOM, например меняем текст профиля и закрываем модальное окно.
-    .catch((err) => {
-      console.log(err); // выводим ошибку в консоль
-    })
-    .finally(() => {
-      renderLoading(false, buttonSaveAvatar);
-    })
-}
-
-formEditAvatar.addEventListener('submit', editAvatar);
-
-export function renderLoading(isLoading, button) {
-  if (isLoading) {
-    button.textContent = 'Сохранение';
+    this._cardSection = null;
+    this._initializePage();
   }
-  else {
-    button.textContent = 'Сохранить';
-    buttonElementCreate.textContent = 'Создать';
+
+  _initializePage() {
+    this._initializeData();
+    this._initializeCommands();
+  }
+
+  _initializeData() {
+    Promise.all([webApi.getUserInfo(), webApi.getInitialCards()])
+      .then(([userData, cardsData]) => {
+        profileInfo.setUserInfo(userData);
+        cardSectionSingleton.initialize('.elements', cardsData)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  _initializeCommands() {
+    popupManager.initializePopup(this._editProfileSelector, (data) => this._submitEditProfileForm(data));
+    popupManager.initializePopup(this._addImageSelector, (data) => this._addCard(data));
+    popupManager.initializePopup(this._editAvatarSelector, (data) => this._editAvatar(data));
+
+    this._buttonOpenEditProfilePopup.addEventListener('click', () => {
+      popupManager.getPopupBySelector(this._editProfileSelector).open(profileInfo.getUserInfo())
+    });
+    this._buttonAddCard.addEventListener('click', () => {
+      popupManager.getPopupBySelector(this._addImageSelector).open()
+    });
+    this._buttonEditAvatar.addEventListener('click', () => {
+      popupManager.getPopupBySelector(this._editAvatarSelector).open(profileInfo.getUserInfo())
+    });
+  }
+  _editAvatar(data) {
+    webApi.updateAvatarInfo({ avatar: data.avatar })
+      .then(data => {
+        profileInfo.setUserInfo(data);
+      })
+      .then(() => {
+        popupManager.getPopupBySelector(this._editAvatarSelector).close();
+      })
+      .catch((err) => {
+        console.log(err);
+      }).finally(() => {
+        popupManager.getPopupBySelector(this._editAvatarSelector).renderLoading(false);
+      })
+  }
+
+  _submitEditProfileForm(data) {
+    webApi.updateProfileInfo({ name: data.name, about: data.about })
+      .then(data => {
+        profileInfo.setUserInfo(data);
+      })
+      .then(() => {
+        popupManager.getPopupBySelector(this._editProfileSelector).close();
+      })
+      .catch((err) => {
+        console.log(err);
+      }).finally(() => {
+
+        popupManager.getPopupBySelector(this._editProfileSelector).renderLoading(false);
+      })
+  }
+  _addCard(data) {
+    webApi.addCard({ name: data.cardName, link: data.cardUri })
+      .then(data => cardSectionSingleton.createCard(data))
+      .then(() => {
+        popupManager.getPopupBySelector(this._addImageSelector).close();
+      })
+      .catch((err) => {
+        console.log(err);
+      }).finally(() => {
+
+        popupManager.getPopupBySelector(this._addImageSelector).renderLoading(false);
+      })
   }
 }
 
-enableValidation(settings);*/
 
-const api = new Api({
-  baseUrl: "https://nomoreparties.co/v1/plus-cohort-23/",
-  headers: {
-    authorization: "70b0f800-c3d5-43c3-9a38-db0198e51959",
-    "content-Type": "application/json",
-  },
-});
+const IndexPage = new IndexPagePresenter();
 
-export let myId = null//Объявляем глобально переменную c id, что мы могли использовать её, например в card.js.
-export let myAbout = null;
-export let myAvatar = null;
-export let myName = null;
-
-function handleCardClick() {
-  popupImage.src = this._link;
-  popupTextImage.textContent = this._name;
-  openPopup(popupOpenCard);
-}
-
-function handleLikeClick() {
-  this.buttonLike.dataset.id = this._id;
-  if (this.buttonLike.classList.toggle('element__button_active')) {
-    api.likePutServer(this.buttonLike.dataset.id = this._id)
-      .then(() => { this.likesNumber.textContent++; })
-  }
-
-  else {
-    api.likeDeleteServer(this.buttonLike.dataset.id = this._id)
-      .then(() => { this.likesNumber.textContent--; })
-  };
-
-  /*if (event.target.classList.toggle('element__button_active')) {
-    likePutServer(buttonLike.dataset.id = id)
-      .then(() => { event.target.nextElementSibling.textContent++; })
-  }
-  else {
-    likeDeleteServer(buttonLike.dataset.id = id)
-      .then(() => { event.target.nextElementSibling.textContent--; })
-  };*/
-}
-
-
-let UserInfoDefault = new UserInfo({
-  userNameSelector: ".profile__info-cell-text",
-  userAboutSelector: ".profile__info-text",
-  avatarSelector: ".profile__image",
-})
-
-Promise.all([api.getUserInfo(), api.getInitialCards()])
-  .then(([userData, cardsData]) => {
-    let myId = userData._id;
-    /*let myAbout = userData.about;
-    let myAvatar = userData.avatar;
-    let myName = userData.name;
-    editAvatarInfo(myAvatar, myName, myAbout);*/
-    UserInfoDefault.setUserInfo(userData);
-    const createCard = (data) => {
-      const card = new Card(
-        data,
-        '#templateElements',
-        handleCardClick,
-        handleLikeClick,
-        myId,
-      );
-      return card.generateCard();
-    }
-
-    let cardList = new Section(
-      {
-        items: [],
-        renderer: createCard,
-      },
-      '.elements'
-    );
-    //console.log(myId);
-    cardList.renderItems(cardsData);
-    /* cardsData.forEach(function (result) {
-       const card = createCard(result.name, result.link,
-         result._id, result.owner._id, result.likes, myId);
-       const elementList = document.querySelector('s');
-       elementList.append(card)
-     })*/
-  })
-/*.catch((err) => {
-  console.log(err); // выводим ошибку в консоль
-});*/
-
-
-/*export function submitEditProfileForm(evt) {
-  evt.preventDefault();
-  const popupEdifProfile = document.querySelector('.popup_edit-profile');
-  const buttonSaveProfile = document.querySelector('.form__button');
-  const nameInput = document.querySelector('.form__name-text');
-  const jobInput = document.querySelector('input:nth-of-type(2)');
-  const nameProfile = document.querySelector('.profile__info-cell-text');
-  const job = document.querySelector('.profile__info-text');
-  renderLoading(true, buttonSaveProfile);
-  profileInfoPatch({ name: nameInput.value, about: jobInput.value })
-    .then(data => {
-      nameProfile.textContent = nameInput.value;
-      job.textContent = jobInput.value;
-      closePopup(popupEdifProfile);
-    })
-    //Здесь вносим изменения в DOM, например меняем текст профиля и закрываем модальное окно.
-    .catch((err) => {
-      console.log(err); // выводим ошибку в консоль
-    })
-    .finally(() => {
-      renderLoading(false, buttonSaveProfile);
-    })
-}
-
-formElement.addEventListener('submit', submitEditProfileForm);
-
-/*export function addCard(evt) {
-  evt.preventDefault();
-  renderLoading(true, buttonElementCreate);
-  addCardServerPost({ name: nameInputImage.value, link: linkInputImage.value })
-    .then(data => {
-      const card = createCard(data.name, data.link,
-        data._id, data.owner._id, data.likes, data.owner._id);
-      elementList.prepend(card);
-    })
-    .catch((err) => {
-      console.log(err); // выводим ошибку в консоль
-    })
-    .finally(() => {
-      renderLoading(false, buttonElementCreate)
-      closePopup(popupAddImage);
-    })
-}*/
 
 
 
