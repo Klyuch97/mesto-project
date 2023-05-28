fef86725e871d857a0ffa21db29efec3407f95ca
AKuznetsov <artemkuznetsov07@gmail.com>
2023-05-28 13:11:44 +0300

    мелкие правки

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,13 +1,5 @@
 'use strict';
 
-//import { Api } from '../api.js';
-//import { ValidationService } from '../Validation/ValidationService.js';
-//import { UserInfo } from '../UserInfo.js';
-//import { apiConfig, validatorSettings, userInfoSettings, cardSectionConfig } from '../utils/constants.js';
-//import { CardSection } from "../Sections/CardSection/CardSection.js";
-//import { PopupManager } from '../Popups/PopupManager.js';
-
-
 export class NodeFactory {
   constructor() { }
   createNodeFromTemplate(id) {
@@ -18,11 +10,3 @@ export class NodeFactory {
     return document.querySelector(id).content;
   }
 };
-
-//export const popupManagerSingleton = PopupManager;
-//export const cardSectionSingleton = CardSection;
-//CardSection.setConfig(cardSectionConfig);
-//export const profileInfo = new UserInfo(userInfoSettings);
-//export const formsValidationService = new ValidationService(validatorSettings);
-//export const webApi = new Api(apiConfig);
-//export const factory = new NodeFactory();
